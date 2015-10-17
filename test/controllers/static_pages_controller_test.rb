b67b7469b33bf6b8962d61d8e7bf661a03d57506
Rob Conrad <robertconrad+git@gmail.com>
2015-10-17 16:51:41 -0700

    Add about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test 'should get home' do
     get :home
     assert_response :success
   end
 
   test "should get help" do
     get :help
+    assert_response :success
+  end
+
+  test 'should get about' do
+    get :about
     assert_response :success
   end
 
