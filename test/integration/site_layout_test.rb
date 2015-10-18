d0b09b5e36898dc85a248771c3c38c37f525b197
Rob Conrad <robertconrad+git@gmail.com>
2015-10-18 16:20:40 -0700

    Add signup template integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
   end
 
+  test 'should get signup' do
+    get signup_path
+    assert_response :success
+    assert_template 'users/new'
+    assert_select 'title', "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select 'h1', 'Sign up'
+  end
+
 end
