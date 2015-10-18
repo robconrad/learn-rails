ad686b1b7d7256321547ea6d6bba4f78a890393b
Rob Conrad <robertconrad+git@gmail.com>
2015-10-18 16:59:37 -0700

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017225220) do
+ActiveRecord::Schema.define(version: 20151018233956) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
