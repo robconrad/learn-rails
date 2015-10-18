edaa6dcadae2902fb91809e515921de53ae953ac
Rob Conrad <robertconrad+git@gmail.com>
2015-10-17 17:25:27 -0700

    Refactor title creation into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base = 'Ruby on Rails Tutorial Sample App'
+    if page_title.length == 0
+      base
+    else
+      "#{page_title} | #{base}"
+    end
+  end
+
 end
