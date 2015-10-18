e9b66fb83cb38c379135be412920803ae47b1961
Rob Conrad <robertconrad+git@gmail.com>
2015-10-18 16:27:36 -0700

    Add direct ApplicationHelper test, use ApplicationHelper as fixture in other tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,20 +2,17 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test 'layout links' do
+  test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select 'a[href=?]', root_path, count: 2
-    assert_select 'a[href=?]', help_path
-    assert_select 'a[href=?]', about_path
-    assert_select 'a[href=?]', contact_path
-  end
-
-  test 'should get signup' do
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
     get signup_path
     assert_response :success
     assert_template 'users/new'
-    assert_select 'title', "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select "title", full_title("Sign up")
     assert_select 'h1', 'Sign up'
   end
 
