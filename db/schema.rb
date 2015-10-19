e98ca948ad5ad61cdd2d920142f5dc50e7594796
Rob Conrad <robertconrad+git@gmail.com>
2015-10-18 18:34:27 -0700

    Add user signup form

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151019004947) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
