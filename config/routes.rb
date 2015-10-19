63642e1ce35ac561a66ec10a3814af7e1bee868b
Rob Conrad <robertconrad+git@gmail.com>
2015-10-18 21:07:45 -0700

    Add Sessions controller for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  root                   'static_pages#home'
 
-  root             'static_pages#home'
+  get     'login'     => 'sessions#new'
+  post    'login'     => 'sessions#create'
+  delete  'logout'    => 'sessions#destroy'
 
-  get 'home'    => 'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get     'users/new'
+
+  get     'home'      => 'static_pages#home'
+  get     'help'      => 'static_pages#help'
+  get     'about'     => 'static_pages#about'
+  get     'contact'   => 'static_pages#contact'
+  get     'signup'    => 'users#new'
 
   resources :users
 
