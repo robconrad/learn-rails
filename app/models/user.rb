46d084757efb9a2dd729e1187d2daf4e5ff171d0
Rob Conrad <robertconrad+git@gmail.com>
2015-10-19 23:16:23 -0700

    Handle good user edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   before_save { self.email.downcase! }
 
