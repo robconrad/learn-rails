5018ba62919e8122c756adc7cf7d3e303b476714
Rob Conrad <robertconrad+git@gmail.com>
2015-10-19 23:15:05 -0700

    Handle bad user edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-
 end
